c45898d01cd4959e399a76b8d7b4a383f0894d4d
Nikolas Heise <heisenikolas@gmail.com>
2024-04-18 22:54:54 +0200

    Initial Commit

--- bot/main.go
+++ bot/main.go
@@ -1,46 +1,53 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "os"
-  "os/signal"
-  "syscall"
+	"flag"
+	"github.com/rs/zerolog/log"
+	"os"
+	"os/signal"
+	"syscall"
 
-  "github.com/bwmarrin/discordgo"
+	"github.com/bwmarrin/discordgo"
 )
 
 var Token string
 
 func init() {
-  flag.StringVar(&Token, "t", "", "The token for your bot")
-  flag.Parse()
+	flag.StringVar(&Token, "t", "", "The discord api bot token for your bot")
+	flag.Parse()
 }
 
 func main() {
-  bot, err := discordgo.New("Bot " + Token)
-  if err != nil {
-    fmt.Println("[ERROR]: creating Discord session,", err)
-    return
-  }
-  bot.AddHandler(messageCreate)
-  err = bot.Open()
-  if err != nil {
-    fmt.Println("[ERROR]: opening connection,", err)
-    return
-  }
-  fmt.Println("[BOT]: Running     Press CTRL-C to exit.")
-  sc := make(chan os.Signal, 1)
-  signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-  <-sc
-  bot.Close()
+
+	log.Info().Msg("Starting UBOT. (c) 2024 Nikolas Heise. https://sonefall.com/")
+	bot, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		log.Fatal().Msg("Fatal error creating discord session, exiting...: " + err.Error())
+		return
+	}
+
+	log.Info().Msg("Discord session initialized. Adding Handlers...")
+
+	bot.AddHandler(messageCreate)
+
+	err = bot.Open()
+	if err != nil {
+		log.Fatal().Msg("Fatal error opening connection, exiting..., " + err.Error())
+		return
+	}
+
+	log.Info().Msg("UBOT is running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+	bot.Close()
 }
 
 func messageCreate(session *discordgo.Session, msg *discordgo.MessageCreate) {
-  if msg.Author.ID == session.State.User.ID {
-    return
-  }
-  if msg.Content == "/hello" {
-    session.ChannelMessageSend(msg.ChannelID, "Hi!")
-  }
+	if msg.Author.ID == session.State.User.ID {
+		return
+	}
+	if msg.Content == "/hello" {
+		session.ChannelMessageSend(msg.ChannelID, "Hi!")
+	}
 }
